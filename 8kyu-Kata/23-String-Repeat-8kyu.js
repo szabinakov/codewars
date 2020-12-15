c21b541394ad96c7ba642dcb5753a2787d69f297
szabinakov <szabinkov@gmail.com>
2020-12-15 12:32:04 +0000

    added more KATA and their solutions

---
+++ 8kyu-Kata/23-String-Repeat-8kyu.js
@@ -0,0 +1,9 @@
+// Write a function called repeat_str which repeats the given string src exactly count times.
+
+// repeatStr(6, "I") // "IIIIII"
+// repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+//Solution:
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
---
+++ 8kyu-Kata/24-Remove-String-Spaces-8kyu.js
@@ -0,0 +1,6 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+//Solution:
+function noSpace(x) {
+  return x.split(" ").join("");
+}
